1488f6b8b6ce6e0ab4a6952b2507d159e8bffadb
Dragnansia <dragnansia.git@tutanota.com>
2023-09-14 00:04:33 +0200

    Use clap crate for parsing argument
    Remove the old argument parser to use clap for more stability and
    simplcity.

---
+++ src/args.rs
@@ -0,0 +1,32 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+pub struct Args {
+    /// Cleaning the cache
+    #[arg(long, default_value_t = false)]
+    pub clear_cache: bool,
+
+    /// Set the cooldown in milliseconds
+    #[arg(short, long, default_value_t = 25)]
+    pub cooldown: u64,
+
+    /// Bind left autoclicker to keycode
+    #[arg(short, long, default_value_t = 275)]
+    pub left_bind: u16,
+
+    /// Bind right autoclicker to keycode
+    #[arg(short, long, default_value_t = 276)]
+    pub right_bind: u16,
+
+    /// For finding what key is pressed
+    #[arg(short, long, default_value_t = false)]
+    pub find_keycodes: bool,
+
+    /// For not beeping when the autoclicker state is changed
+    #[arg(short, long, default_value_t = false)]
+    pub no_beep: bool,
+
+    #[arg(long, default_value_t = false)]
+    pub debug: bool,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,107 +1,36 @@
-use std::{fs, process::abort};
+pub mod args;
+pub mod device;
 
-use input_linux::Key;
+use std::fs;
+
+use clap::Parser;
 use theclicker::State;
 
+use crate::args::Args;
+
 fn main() {
-    let mut cooldown = 25;
-    let mut debug = false;
-    let mut find_keycodes = false;
-    let mut args: Vec<String> = Vec::new();
-    let mut left_bind: u16 = Key::ButtonSide.into();
-    let mut right_bind: u16 = Key::ButtonExtra.into();
+    let parse = Args::parse();
+
     let mut beep = true;
 
-    for arg in std::env::args() {
-        args.push(arg);
+    if parse.clear_cache {
+        let _ = fs::remove_file("/tmp/TheClicker");
     }
 
-    for (index, arg) in std::env::args().enumerate() {
-        if index > 0 {
-            match arg.as_str() {
-                "--clear-cache" => {
-                    let _ = fs::remove_file("/tmp/TheClicker");
-                }
-                "--cooldown" => {
-                    if let Some(arg) = args.get(index + 1) {
-                        let num = match arg.trim().parse::<u64>() {
-                            Ok(num) => num,
-                            Err(_) => {
-                                eprintln!("Specify a number!");
-                                abort()
-                            }
-                        };
-                        cooldown = num;
-                    } else {
-                        println!("After --cooldown specify the cooldown");
-                        abort()
-                    }
-                }
-                "--help" => {
-                    println!("--clear-cache for cleaning the cache!");
-                    println!("--cooldown [num] for set the cooldown in milliseconds!");
-                    println!("--left-bind [keycode] bind left autoclicker to keycode");
-                    println!("--right-bind [keycode] bind right autoclicker to keycode");
-                    println!("--find-keycodes for finding what key is press");
-                    println!("--no-beep for not beeping when the autoclicker state is changed");
-                    return;
-                }
-                "--debug" => {
-                    debug = true;
-                    println!("Debugging!");
-                }
-                "--left-bind" => {
-                    if let Some(arg) = args.get(index + 1) {
-                        let num = match arg.trim().parse::<u16>() {
-                            Ok(num) => num,
-                            Err(_) => {
-                                println!("Specify a keycode number!");
-                                return;
-                            }
-                        };
-                        left_bind = num;
-                        println!("Left autoclicker is bind on {}", num);
-                    } else {
-                        eprintln!("No keycode after --left-bind");
-                        abort()
-                    }
-                }
-                "--right-bind" => {
-                    if let Some(arg) = args.get(index + 1) {
-                        let num = match arg.trim().parse::<u16>() {
-                            Ok(num) => num,
-                            Err(_) => {
-                                println!("Specify a keycode number!");
-                                return;
-                            }
-                        };
-                        right_bind = num;
-                        println!("Right autoclicker is bind on {}", num);
-                    } else {
-                        eprintln!("No keycode after --right-bind");
-                        abort()
-                    }
-                }
-                "--find-keycodes" => {
-                    find_keycodes = true;
-                    println!("Autoclicker inactive!");
-                }
-                "--no-beep" => {
-                    beep = false;
-                    println!("Beep off");
-                }
-                _ => {
-                    println!("Invalid argument: {arg}");
-                    abort();
-                }
-            }
-        }
+    if parse.no_beep {
+        beep = false;
     }
 
-    let state = State::new(cooldown, debug, find_keycodes, left_bind, right_bind, beep);
+    let state = State::new(
+        parse.cooldown,
+        parse.debug,
+        parse.find_keycodes,
+        parse.left_bind,
+        parse.right_bind,
+        beep,
+    );
     println!("Launched!\n");
-
-    println!("Cooldown is set to {}ms!", cooldown);
+    println!("Cooldown is set to {}ms!", parse.cooldown);
 
     state.main_loop();
 }
